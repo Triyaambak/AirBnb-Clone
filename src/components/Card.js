3e4f62796171cc6de67be132eb2b2a5d2825a732
Triyaambak <triyaambaks2003@gmail.com>
2023-12-09 21:48:40 +0530

    9/12/23

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div>
             <div className="card">
-                <img src={require('../images/body1.png')} alt="" className="card-image"></img>
+                <img src={props.img} alt="" className="card-image"></img>
                 <div className="card-stats">    
                         <img src={require('../images/Star.png')} className="card-star" alt=""></img>
-                    <span>5.0</span>
-                    <span className="grey">(6) • </span>
-                    <span className="grey">USA</span>
+                    <span>{props.rating}</span>
+                    <span className="grey">({props.reviewCount}) • </span>
+                    <span className="grey">{props.country}</span>
                 </div>
-                <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+                <p>{props.title}</p>
+                <p><span className="bold">From ${props.price}</span> / person</p>
             </div>
         </div>
     )
